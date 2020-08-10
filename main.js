d2117171d6021680206f32049168bb347b1a42e5
David Lucas <davidlucasmora81@gmail.com>
2020-08-10 13:44:57 +0200

    Un poco de javascript

---
+++ main.js
@@ -0,0 +1,14 @@
+let mailButton = document.getElementById("mailButton")
+let mailBox = document.getElementById("mailBox")
+
+function sendEmail(){
+	let mail = mailBox.value
+	if(!mail || !mail.includes("@") || !mail.includes(".")){
+		alert("Introduzca un mail valido: nombre@dominio")
+	}
+	else{
+		alert("El correo "+mail+" ha sido almacenado satisfactoriamente")
+	}
+	
+	document.activeElement.blur()
+}
